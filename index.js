b0ff1dc8b04d3490ae762e332af9ee40a2e4e53f
Justin Oeters <justin.oeters@gmail.com>
2021-11-11 07:24:32 -0500

    page is working

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+//Handlebars set up
+app.set("view engine", "hbs");
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'index'
+ }));
+
 //router set up
 const PORT = 3001;
 app.listen(PORT);
@@ -13,5 +20,5 @@ console.log(`Now listening to Server: http://localhost:${PORT}`);
 
 //landing page
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.render("main");
 });
